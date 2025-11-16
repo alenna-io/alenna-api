9e1ffdce607b0b18dc2afc35875c50a3eea7ad1a
sgarnica1 <sgarnica1902@gmail.com>
2025-11-15 19:24:58 -0600

    feat: update prisma db config

--- src/core/frameworks/database/prisma.client.ts
+++ src/core/frameworks/database/prisma.client.ts
@@ -2,6 +2,16 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({
   log: process.env.NODE_ENV === 'development' ? ['error', 'warn'] : ['error'],
+  datasources: {
+    db: {
+      url: process.env.DATABASE_URL,
+    },
+  },
+});
+
+// Graceful shutdown
+process.on('beforeExit', async () => {
+  await prisma.$disconnect();
 });
 
 export default prisma;
